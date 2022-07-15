bd12554bad17d7601fa3bf29d6cf2649ee7a68d6
Terry Brady <terrence.brady@ucop.edu>
2022-07-15 15:30:03 -0700

    initial inv-it resources

---
+++ inv-it/src/test/README.md
@@ -0,0 +1,14 @@
+## To test with maven
+
+The maven plugin will assign random high port numbers to the containers that have been started.
+```
+mvn verify
+```
+
+## To run from the command line or in a debugger
+
+```
+MDIR=$(pwd) docker-compose -f inv-it/src/test/docker/docker-compose.yml up -d
+```
+
+Run the junit test
---
+++ inv-it/src/test/java/org/cdlib/mrt/inv/ServiceDriverIT.java
@@ -0,0 +1,115 @@
+package org.cdlib.mrt.inv;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+
+import javax.xml.xpath.XPathFactory;
+//https://stackoverflow.com/a/22939742/3846548
+import org.apache.xpath.jaxp.XPathFactoryImpl;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.Assert.*;
+
+public class ServiceDriverIT {
+        private int port = 8080;
+        private String cp = "mrtinv";
+        private DocumentBuilder db;
+        private XPathFactory xpathfactory;
+
+        public ServiceDriverIT() throws ParserConfigurationException {
+                try {
+                        port = Integer.parseInt(System.getenv("it-server.port"));
+                } catch (NumberFormatException e) {
+                        System.err.println("it-server.port not set, defaulting to " + port);
+                }
+                db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                xpathfactory = new XPathFactoryImpl();
+        }
+
+        public String getContent(String url, int status) throws HttpResponseException, IOException {
+                try (CloseableHttpClient client = HttpClients.createDefault()) {
+                    HttpGet request = new HttpGet(url);
+                    HttpResponse response = client.execute(request);
+                    if (status > 0) {
+                        assertEquals(status, response.getStatusLine().getStatusCode());
+                    }
+
+                    if (status > 300) {
+                        return "";
+                    }
+                    String s = new BasicResponseHandler().handleResponse(response).trim();
+                    assertFalse(s.isEmpty());
+                    return s;
+                }
+        }
+
+        public JSONObject getJsonContent(String url, int status) throws HttpResponseException, IOException, JSONException {
+                String s = getContent(url, status);
+                JSONObject json =  new JSONObject(s);
+                assertNotNull(json);
+                return json;
+        }
+
+        public List<String> getZipContent(String url, int status) throws HttpResponseException, IOException {
+                try (CloseableHttpClient client = HttpClients.createDefault()) {
+                    HttpGet request = new HttpGet(url);
+                    HttpResponse response = client.execute(request);
+                    assertEquals(status, response.getStatusLine().getStatusCode());
+
+                    List<String> entries = new ArrayList<>();
+                    if (status < 300) {
+                            try(ZipInputStream zis = new ZipInputStream(response.getEntity().getContent())){
+                                    for(ZipEntry ze = zis.getNextEntry(); ze != null; ze = zis.getNextEntry()) {
+                                            entries.add(ze.getName());
+                                    }
+                            }
+                    }
+
+                    return entries;
+                }
+        }
+
+
+        @Test
+        public void SimpleTest() throws IOException, JSONException {
+                String url = String.format("http://localhost:%d/%s/state?t=json", port, cp);
+                JSONObject json = getJsonContent(url, 200);
+                assertTrue(json.has("invsv:invServiceState"));
+        }
+}
