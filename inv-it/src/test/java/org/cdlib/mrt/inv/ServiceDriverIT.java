b20291df6faf090741f533d47748c29eff1102c7
Terry Brady <terrence.brady@ucop.edu>
2022-07-28 12:42:14 -0700

    refine tests

--- inv-it/src/test/java/org/cdlib/mrt/inv/ServiceDriverIT.java
+++ inv-it/src/test/java/org/cdlib/mrt/inv/ServiceDriverIT.java
@@ -1,10 +1,8 @@
 package org.cdlib.mrt.inv;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -14,43 +12,24 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.FormBodyPartBuilder;
-import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
 import java.net.URLEncoder;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
 
 import javax.xml.xpath.XPathFactory;
 //https://stackoverflow.com/a/22939742/3846548
 import org.apache.xpath.jaxp.XPathFactoryImpl;
-import org.glassfish.jersey.media.multipart.FormDataMultiPart;
-
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
 
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
 
 import static org.junit.Assert.*;
 
@@ -70,13 +49,11 @@ public class ServiceDriverIT {
                 }
                 db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
                 xpathfactory = new XPathFactoryImpl();
-
+                initServiceAndNodes();
         }
 
-        @Before
         public void initServiceAndNodes() throws IOException, JSONException {
                 String url = String.format("http://localhost:%d/%s/service/start?t=json", port, cp);
-                System.out.println(url);
                 try (CloseableHttpClient client = HttpClients.createDefault()) {
                         HttpPost post = new HttpPost(url);
                         HttpResponse response = client.execute(post);
@@ -84,8 +61,8 @@ public class ServiceDriverIT {
                         String s = new BasicResponseHandler().handleResponse(response).trim();
                         assertFalse(s.isEmpty());
 
-                        System.out.println(s);
                         JSONObject json =  new JSONObject(s);
+                        assertNotNull(json);
                 }
                 setFileNode(primaryNode);
                 setFileNode(replNode);
@@ -110,30 +87,10 @@ public class ServiceDriverIT {
 
         public JSONObject getJsonContent(String url, int status) throws HttpResponseException, IOException, JSONException {
                 String s = getContent(url, status);
-                JSONObject json =  new JSONObject(s);
+                JSONObject json = s.isEmpty() ? new JSONObject() : new JSONObject(s);
                 assertNotNull(json);
                 return json;
         }
-
-        public List<String> getZipContent(String url, int status) throws HttpResponseException, IOException {
-                try (CloseableHttpClient client = HttpClients.createDefault()) {
-                    HttpGet request = new HttpGet(url);
-                    HttpResponse response = client.execute(request);
-                    assertEquals(status, response.getStatusLine().getStatusCode());
-
-                    List<String> entries = new ArrayList<>();
-                    if (status < 300) {
-                            try(ZipInputStream zis = new ZipInputStream(response.getEntity().getContent())){
-                                    for(ZipEntry ze = zis.getNextEntry(); ze != null; ze = zis.getNextEntry()) {
-                                            entries.add(ze.getName());
-                                    }
-                            }
-                    }
-
-                    return entries;
-                }
-        }
-
 
         @Test
         public void SimpleTest() throws IOException, JSONException {
@@ -141,11 +98,13 @@ public class ServiceDriverIT {
                 JSONObject json = getJsonContent(url, 200);
                 assertTrue(json.has("invsv:invServiceState"));
                 assertEquals("running", json.getJSONObject("invsv:invServiceState").get("invsv:systemStatus"));
+                assertEquals("running", json.getJSONObject("invsv:invServiceState").get("invsv:zookeeperStatus"));
+                assertEquals("running", json.getJSONObject("invsv:invServiceState").get("invsv:dbStatus"));
+                
         }
 
         public JSONObject setFileNode(int node) throws IOException, JSONException {
                 String url = String.format("http://localhost:%d/%s/filenode/%d?t=json", port, cp, node);
-                System.out.println(url);
                 try (CloseableHttpClient client = HttpClients.createDefault()) {
                         HttpPost post = new HttpPost(url);
                         HttpResponse response = client.execute(post);
@@ -157,17 +116,15 @@ public class ServiceDriverIT {
                 }
         }
 
-        @Test
-        public void AddObjectTest() throws IOException, JSONException {
-                String ark = "ark:/1111/2222";
-                String ark_de = URLEncoder.encode(URLEncoder.encode(ark, StandardCharsets.UTF_8.name()), StandardCharsets.UTF_8.name());
-                String manifest = String.format("http://mock-merritt-it:4567/static/storage/manifest/7777/%s", ark_de);
+        public JSONObject addObject(String ark) throws IOException, JSONException {
+                String ark_e = URLEncoder.encode(ark, StandardCharsets.UTF_8.name());
+                String manifest = String.format("http://mock-merritt-it:4567/static/storage/manifest/7777/%s", ark_e);
                 String url = String.format("http://localhost:%d/%s/add", port, cp);
-                System.out.println(url);
                 try (CloseableHttpClient client = HttpClients.createDefault()) {
                         HttpPost post = new HttpPost(url);
                         
                         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                        builder.addTextBody("responseForm", "json", ContentType.TEXT_PLAIN.withCharset("UTF-8"));
                         builder.addTextBody("url", manifest, ContentType.TEXT_PLAIN.withCharset("UTF-8"));
                         HttpEntity entity = builder.build();
                         post.setEntity(entity);
@@ -175,29 +132,198 @@ public class ServiceDriverIT {
                         assertEquals(200, response.getStatusLine().getStatusCode());
                         String s = new BasicResponseHandler().handleResponse(response).trim();
                         assertFalse(s.isEmpty());
-
-                        System.out.println(s);
+                        JSONObject json = new JSONObject(s);
+                        assertTrue(json.has("invp:invProcessState"));
+                        return json;
                 }
         }
 
-        public void DeleteObjectTest() {
-                //DELETE @Path("object/{objectIDS}")
+        public JSONObject setLocalIdByFormParams(String ark, String owner, String localid) throws IOException, JSONException {
+                String url = String.format("http://localhost:%d/%s/primary", port, cp);
+                try (CloseableHttpClient client = HttpClients.createDefault()) {
+                        HttpPost post = new HttpPost(url);
+                        
+                        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                        builder.addTextBody("response-form", "json", ContentType.TEXT_PLAIN.withCharset("UTF-8"));
+                        builder.addTextBody("objectid", ark, ContentType.TEXT_PLAIN.withCharset("UTF-8"));
+                        builder.addTextBody("ownerid", owner, ContentType.TEXT_PLAIN.withCharset("UTF-8"));
+                        builder.addTextBody("localids", localid, ContentType.TEXT_PLAIN.withCharset("UTF-8"));
+                        HttpEntity entity = builder.build();
+                        post.setEntity(entity);
+                        HttpResponse response = client.execute(post);
+                        assertEquals(200, response.getStatusLine().getStatusCode());
+                        String s = new BasicResponseHandler().handleResponse(response).trim();
+                        assertFalse(s.isEmpty());
+                        JSONObject json = new JSONObject(s);
+                        assertTrue(json.has("invloc:localContainerState"));
+                        assertEquals(owner, json.getJSONObject("invloc:localContainerState").getString("invloc:ownerID"));
+                        assertEquals(localid, json.getJSONObject("invloc:localContainerState").getString("invloc:localIDs"));
+                        return json;
+                }
         }
 
-        public void SetLocalIdsTest() {
-                //POST @Path("primary/{objectIDS}/{ownerIDS}/{localIDs}")
-
+        public JSONObject setLocalId(String ark, String owner, String localid) throws IOException, JSONException {
+                String url = String.format("http://localhost:%d/%s/primary/%s/%s/%s?t=json", 
+                        port,
+                        cp,
+                        URLEncoder.encode(ark, StandardCharsets.UTF_8.name()),
+                        URLEncoder.encode(owner, StandardCharsets.UTF_8.name()),
+                        URLEncoder.encode(localid, StandardCharsets.UTF_8.name())
+                );
+                try (CloseableHttpClient client = HttpClients.createDefault()) {
+                        HttpPost post = new HttpPost(url);
+                        HttpResponse response = client.execute(post);
+                        assertEquals(200, response.getStatusLine().getStatusCode());
+                        String s = new BasicResponseHandler().handleResponse(response).trim();
+                        assertFalse(s.isEmpty());
+                        JSONObject json = new JSONObject(s);
+                        assertTrue(json.has("invloc:localContainerState"));
+                        assertEquals(owner, json.getJSONObject("invloc:localContainerState").getString("invloc:ownerID"));
+                        assertEquals(localid, json.getJSONObject("invloc:localContainerState").getString("invloc:localIDs"));
+                        return json;
+                }
         }
 
-        public void SetLocalIdsFormParamTest() {
-                //POST @Path("primary")
-                //@DefaultValue("") @FormDataParam("objectid") String objectIDS,
-                //@DefaultValue("") @FormDataParam("ownerid") String ownerIDS,
-                //@DefaultValue("") @FormDataParam("localids") String localIDs,
+        public JSONObject deleteObject(String ark) throws IOException, JSONException {
+                String url = String.format("http://localhost:%d/%s/object/%s?t=json",
+                        port,
+                        cp,
+                        URLEncoder.encode(ark, StandardCharsets.UTF_8.name())
+                );
+                try (CloseableHttpClient client = HttpClients.createDefault()) {
+                        HttpDelete del = new HttpDelete(url);
+                        HttpResponse response = client.execute(del);
+                        assertEquals(200, response.getStatusLine().getStatusCode());
+                        String s = new BasicResponseHandler().handleResponse(response).trim();
+                        assertFalse(s.isEmpty());
+                        JSONObject json = new JSONObject(s);
+                        assertTrue(json.has("invd:invDeleteState"));
+                        return json;
+                }
         }
 
-        public void getLocalIdsTest() {
-                //@GET @Path("local/{objectIDS}")
+        public JSONObject deleteLocalids(String ark) throws IOException, JSONException {
+                String url = String.format("http://localhost:%d/%s/primary/%s?t=json",
+                        port,
+                        cp,
+                        URLEncoder.encode(ark, StandardCharsets.UTF_8.name())
+                );
+                try (CloseableHttpClient client = HttpClients.createDefault()) {
+                        HttpDelete del = new HttpDelete(url);
+                        HttpResponse response = client.execute(del);
+                        assertEquals(200, response.getStatusLine().getStatusCode());
+                        String s = new BasicResponseHandler().handleResponse(response).trim();
+                        assertFalse(s.isEmpty());
+                        JSONObject json = new JSONObject(s);
+                        assertTrue(json.has("invloc:localContainerState"));
+                        return json;
+                }
+        }
+
+        public boolean checkArk(String ark) throws HttpResponseException, IOException, JSONException {
+                String url = String.format("http://localhost:%d/%s/manurl/%s?t=json", 
+                        port, 
+                        cp, 
+                        URLEncoder.encode(ark, StandardCharsets.UTF_8.name())
+                );
+                try{
+                        JSONObject json = getJsonContent(url, 0);
+                        JSONObject j = json.has("invman:invManifestUrl") ? json.getJSONObject("invman:invManifestUrl") : new JSONObject();
+                        String ja = j.has("invman:objectID") ? j.getString("invman:objectID") : "";
+                        assertEquals(ark, ja);
+                        return true;
+                } catch(Exception e) {
+                        return false;
+                }
+        }
+
+        @Test
+        public void AddObjectTest() throws IOException, JSONException {
+                String ark = "ark:/1111/2222";
+                if (checkArk(ark)) {
+                        deleteObject(ark);
+                }
+                assertFalse(checkArk(ark));
+                addObject(ark);
+                assertTrue(checkArk(ark));
+                deleteObject(ark);
+        }
+
+        @Test
+        public void AddObjectWithLocalIdTest() throws IOException, JSONException {
+                String ark = "ark:/1111/2222";
+                String localid = "localid";
+                String owner = "owner";
+                if (checkArk(ark)) {
+                        deleteObject(ark);
+                        deleteLocalids(ark);
+                }
+                assertFalse(checkArk(ark));
+                addObject(ark);
+                assertTrue(checkArk(ark));
+                getLocalids(localid, owner, false, ark);
+                getLocalidsByArk(ark, false);
+                setLocalId(ark, owner, localid);
+                getLocalids(localid, owner, true, ark);
+                getLocalidsByArk(ark, true);
+                deleteObject(ark);
+                deleteLocalids(ark);
+                getLocalids(localid, owner, false, ark);
+                getLocalidsByArk(ark, false);
+        }
+
+        @Test
+        public void AddObjectWithLocalIdByFormParamTest() throws IOException, JSONException {
+                String ark = "ark:/1111/2222";
+                String localid = "localid";
+                String owner = "owner";
+                if (checkArk(ark)) {
+                        deleteObject(ark);
+                        deleteLocalids(ark);
+                }
+                assertFalse(checkArk(ark));
+                addObject(ark);
+                assertTrue(checkArk(ark));
+                getLocalids(localid, owner, false, ark);
+                getLocalidsByArk(ark, false);
+                setLocalIdByFormParams(ark, owner, localid);
+                getLocalids(localid, owner, true, ark);
+                getLocalidsByArk(ark, true);
+                deleteObject(ark);
+                deleteLocalids(ark);
+                getLocalids(localid, owner, false, ark);
+                getLocalidsByArk(ark, false);
+        }
+
+        public JSONObject getLocalids(String localid, String owner, boolean expectToFind, String ark) throws HttpResponseException, IOException, JSONException {
+                String url = String.format("http://localhost:%d/%s/primary/%s/%s?t=json", 
+                        port, 
+                        cp, 
+                        URLEncoder.encode(owner, StandardCharsets.UTF_8.name()),
+                        URLEncoder.encode(localid, StandardCharsets.UTF_8.name())
+                );
+                JSONObject json = getJsonContent(url, 200);
+                verifyLocalIdResponse(json, expectToFind, ark);
+                return json;
+        }
+
+        public JSONObject getLocalidsByArk(String ark, boolean expectToFind) throws HttpResponseException, IOException, JSONException {
+                String url = String.format("http://localhost:%d/%s/local/%s?t=json", 
+                        port, 
+                        cp, 
+                        URLEncoder.encode(ark, StandardCharsets.UTF_8.name())
+                );
+                JSONObject json = getJsonContent(url, 200);
+                verifyLocalIdResponse(json, expectToFind, ark);
+                return json;
+        }
+
+        public void verifyLocalIdResponse(JSONObject json, boolean expectToFind, String ark) throws JSONException {
+                assertTrue(json.has("invloc:localContainerState"));
+                assertEquals(expectToFind, json.getJSONObject("invloc:localContainerState").getBoolean("invloc:exists"));
+                if (expectToFind) {
+                        assertEquals(ark, json.getJSONObject("invloc:localContainerState").get("invloc:primaryIdentifier"));
+                }
         }
 
         public void addZookeeperTest() {
@@ -205,4 +331,5 @@ public class ServiceDriverIT {
 
                 //test read from zookeeper
         }
+
 }
